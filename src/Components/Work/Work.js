2d370e2507cc216857ad5be44b6372e248bce3f0
Harsh <harshpatel071@gmail.com>
2020-04-14 21:37:23 -0700

    Refactored and changed website theme

--- src/Components/Work/Work.js
+++ src/Components/Work/Work.js
@@ -3,22 +3,13 @@ import './Work.css';
 
 
 export default function Work(props){
-    const [clicked, setClicked] = useState(false);
-    const handleClick = (e) => {
-        setClicked(!clicked);
-        console.log(clicked)
-    }
-    const workDescriptionPoints = props.description.split('\n');
 
     return (
-        <div onClick={handleClick}>
-        <div className={`work-content ${clicked ? 'selected' : ' '}`}>
+        <div>
+        <div className={`work-content`}>
             {<div className='work-place' >{props.place}</div>}
         </div>
-         <div className={`work-description ${clicked ? '' : 'hidden'}`}>{
-             workDescriptionPoints.map((description)=><div className={'description'}>{'•'+description}</div>
-             )
-         } </div>
+
         </div>
     )
 }
--- src/Components/WorkExperience/WorkExperience.jsx
+++ src/Components/WorkExperience/WorkExperience.jsx
@@ -10,20 +10,12 @@ export default function AboutMe(props) {
             <div className='work-container'>
                 <Work className='work'
                       place={'Success and Logistics Specialist | SkipTheDishes March 2018 – Present'}
-                      description={
-                          '\tProvided quality customer service while selling tickets, ensuring safety of pedestrians as well as moving vehicles. \n' +
-                          '\tInstructed cars where to park and responding to all enquiries to provide support and guidance. \n' +
-                          '\tEnsured communication between the traffic team is efficient and communicating with all teams to ensure traffic flows smoothly.'
-                      }
                 />
             </div>
 
             <div className='work-container'>
                 <Work className='work'
                       place={'React developer | Lotlinx January 2020 – Present'}
-                      description={
-                          'In-depth understanding of Javascript, the DOM, and relevant concepts'
-                      }
                 />
             </div>
 

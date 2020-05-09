25cd09abe86440ed4cff6f3c2077c8fa14cd2e52
Harsh <harshpatel071@gmail.com>
2020-05-08 23:25:30 -0700

    Updates

---
+++ precache-manifest.b9d1b335688fdde7141dcdc8bf5b2c5c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0cb2cd625fb01ea7eacbe33883eec0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b48812e7c3c38f9192a",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "040ec079d95e55fe99ad",
+    "url": "/static/css/main.5be481c0.chunk.css"
+  },
+  {
+    "revision": "0b48812e7c3c38f9192a",
+    "url": "/static/js/2.c90b43ae.chunk.js"
+  },
+  {
+    "revision": "8e87f68634b1e5b84f6f7c9aeab3aa68",
+    "url": "/static/js/2.c90b43ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "040ec079d95e55fe99ad",
+    "url": "/static/js/main.201beead.chunk.js"
+  },
+  {
+    "revision": "d304f2ea2d888154816c",
+    "url": "/static/js/runtime-main.a3e097f4.js"
+  }
+]);

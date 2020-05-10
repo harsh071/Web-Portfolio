9a56183ec8230d109d173d4e8290db79282d99bd
Harsh <harshpatel071@gmail.com>
2020-05-09 19:17:36 -0700

    fixed description

--- src/Components/Project/Projects.js
+++ src/Components/Project/Projects.js
@@ -19,7 +19,7 @@ class Projects extends React.Component {
                 <div className='projects-header'>PROJECTS</div>
                 <ProjectListItem projectName={'Chatbot'} description={'A webpage chatbot that uses DiagFlow to process natural language and is hosted on Heroku.'} projectLanguage={'React, DiagFlow, Heroku, MongoDB, Node.js, Git.'}/>
                 <ProjectListItem projectName={'BetMates'} description={' Built parts of an android application that allows users to create and bet with their friends or against them where they can partake in friendly competition. '} projectLanguage={'Java, XML, HSQLDB'}/>
-                <ProjectListItem projectName={'Satsang Insight: Journey to Spirituality in progress '} description={'Assists people in the community to improve their spiritual journey. Consists of a personalized\n' +
+                <ProjectListItem projectName={'Satsang Insight: Journey to Spirituality '} description={'Assists people in the community to improve their spiritual journey. Consists of a personalized\n' +
                 'feed of media and reminders to achieve daily spiritual goals.'} projectLanguage={'React Native, MongoDB'}/>
                 <Link to={'/Web-Portfolio/ProjectGrid'} className='projects-button'>Other Projects</Link>
 
